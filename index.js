f7e9c2089179b14ed0482a676ccb1e13a4c575a1
thecreatorgrey <graybear150@gmail.com>
2024-04-04 11:57:32 -0400

    Big funny

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const board = document.getElementById("board");
 
 const projects = {
+    "This Website":{
+        "year":"2024",
+        "banner":"banners/hamper.png",
+        "tagline":"Not much more to say here",
+        "description":"Not much more to say here",
+        "link":"https://portfolio.thecreatorgrey.site"
+    },
+
     "Space Climb Reborn":{
-        "releaseYear":"2022",
+        "year":"2022",
         "banner":"banners/space climb.png",
         "tagline":"A randomly-generated strategy game",
         "description":"A randomly-generated strategy game.",
@@ -10,19 +18,27 @@ const projects = {
     },
 
     "Mine Thing":{
-        "releaseYear":"2023",
+        "year":"2023",
         "banner":"banners/mine thing.png",
-        "tagline":"A randomly-generated strategy game",
-        "description":"A randomly-generated strategy game.",
+        "tagline":"A simple Minecraft clone",
+        "description":"A simple Minecraft clone.",
         "link":"https://portfolio.thecreatorgrey.site/mine-thing"
     },
 
-    "Kodiak":{
-        "releaseYear":"2023",
-        "banner":"banners/kodiak.png",
-        "tagline":"A randomly-generated strategy game",
-        "description":"A randomly-generated strategy game.",
+    "Kodiak WE":{
+        "year":"2024",
+        "banner":"banners/kodiak WE.png",
+        "tagline":"A web port of the Kodiak engine",
+        "description":"A web port of the Kodiak engine.",
         "link":"https://portfolio.thecreatorgrey.site/kodiak/studio"
+    },
+
+    "Kodiak":{
+        "year":"2023",
+        "banner":"banners/kodiak.png",
+        "tagline":"currently unavailable",
+        "description":"A game engine built with Ursina",
+        "link":null
     }
 }
 
@@ -32,9 +48,9 @@ for (p in projects) {
     data = projects[p];
 
     board.innerHTML += `
-    <span class="card" onclick="window.open('${data.link}', '_blank').focus()">
+    <span class="card" onclick="if ('${data.link}' !== 'null') {window.open('${data.link}', '_blank').focus()}">
         <img src="${data.banner}" alt="banner">
-        <span>${p} (${data.releaseYear})</span><br>
+        <span>${p} (${data.year})</span><br>
         <span style="font-size: 15px">${data.tagline}</span>
     </span>
 `
